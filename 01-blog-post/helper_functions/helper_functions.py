fb522b01ca49408b05b9085551e2d691710c91cd
andrewnachtigal <ajnacht@gmail.com>
2022-05-24 22:58:58 -0700

    readme

--- 01-blog-post/helper_functions/helper_functions.py
+++ 01-blog-post/helper_functions/helper_functions.py
@@ -28,3 +28,23 @@ def replace_values_in_string(text, args_dict):
     for key in args_dict.keys():
         text = text.replace(key, str(args_dict[key]))
     return text
+
+
+# correlation matrix plot
+# https://stackoverflow.com/questions/48139899/correlation-matrix-plot-with-coefficients-on-one-side-scatterplots-on-another
+
+def corrdot(*args, **kwargs):
+    '''create correlation matrix with coefficients, etc.
+
+    Args: dataframe numerical values
+    Returns: corr plot'''
+    corr_r = args[0].corr(args[1], 'pearson')
+    corr_text = f"{corr_r:2.2f}".replace("0.", ".")
+    ax = plt.gca()
+    ax.set_axis_off()
+    marker_size = abs(corr_r) * 10000
+    ax.scatter([.5], [.5], marker_size, [corr_r], alpha=0.6, cmap="coolwarm",
+               vmin=-1, vmax=1, transform=ax.transAxes)
+    font_size = abs(corr_r) * 40 + 5
+    ax.annotate(corr_text, [.5, .5,],  xycoords="axes fraction",
+                ha='center', va='center', fontsize=font_size)
